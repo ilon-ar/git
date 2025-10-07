61bca0bd8e44794a75d8b0c925bfcd3abd8e7de9
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-07 19:15:04 +0700

    ex1 try2

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    int array[10];
-    int sum = 0;
+    float array[10];
+    float sum = 0.0;
     float sr = 0.0;
     for(int i = 0; i < 10; ++i) {
-        scanf("%d", &array[i]);
+        scanf("%f", &array[i]);
         sum += array[i];
     }
     sr = sum / 10.0;
-    int sr_dr = (sr - (int)sr) * 100;
-    printf("%f %d\n", sr, sr_dr);
-    sr = (int)sr + (sr_dr)/100.0;
-    printf("%f\n", sr);
+    int nigga = 0; 
+    if (sr < 0) {
+        sr *= (-1);
+        nigga = 1;
+    } 
+        int sr_dr = (sr - (int)sr) * 1000.0;
+
+    if (sr_dr % 10 >= 5) {
+        sr_dr = (sr_dr / 10) + 1;
+    } else sr_dr /= 10;
+
+
+    if (nigga == 1) {
+        printf("-%d.%d\n", (int)sr, sr_dr);
+    } else {
+        printf("%d.%d\n", (int)sr, sr_dr);
+    }
+    
     return 0;
 }
