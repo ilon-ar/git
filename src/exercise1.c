c3d7bf643007f379ab60432e63681aab214500c3
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-07 19:20:55 +0700

    ex1 try3

--- src/exercise1.c
+++ src/exercise1.c
@@ -14,13 +14,20 @@ int main(int argc, char** argv) {
         sr *= (-1);
         nigga = 1;
     } 
-        int sr_dr = (sr - (int)sr) * 1000.0;
+        int sr_dr = (sr - (int)sr) * 100000.0;
 
-    if (sr_dr % 10 >= 5) {
+    while (sr_dr > 100)
+    {
+        if (sr_dr % 10 >= 5) {
         sr_dr = (sr_dr / 10) + 1;
     } else sr_dr /= 10;
+    }
+    
+        
 
 
+
+    printf("%f %d %d\n", sr, sr_dr, (int)sr);
     if (nigga == 1) {
         printf("-%d.%d\n", (int)sr, sr_dr);
     } else {
