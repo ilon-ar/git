1b3e7483367d55fd521418ceb1e3d88f8b2b03b0
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-07 20:16:51 +0700

    ex1 try7

--- src/exercise1.c
+++ src/exercise1.c
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 
 
 
-    printf("%f %d %d\n", sr, sr_dr, (int)sr);
+    //printf("%f %d %d\n", sr, sr_dr, (int)sr);
     if (nigga == 1) {
         printf("-%d.%d\n", (int)sr, sr_dr);
     } else {
