c88925c5f631a754e6f6448bd5a29d83b1a4a024
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-16 21:27:14 +0700

    ex2 try1

--- src/exercise2.c
+++ src/exercise2.c
@@ -8,31 +8,7 @@ int main(int argc, char** argv) {
     }
     // printf("\n\n\n");
     for (i = i -1; i >= 0; --i) {
-        float x = array[i];
-        if ((int)x == x) {
-            printf("%d ", (int)(x));
-        } else {
-            char neg = 0;
-            if (x < 0) {
-                neg = 1;
-                x *= (-1);
-            }
-            int ccc = (int)x;
-            int dcc = (x - ccc) * 1000;
-            if (dcc % 10 >= 5){
-                dcc = (dcc / 10) + 1;
-            } else dcc /= 10;
-            if ((neg == 0) & (dcc < 10)) {
-                printf("%d.0%d ", ccc, dcc);
-            } else if ((neg == 0) & (dcc > 10)){
-                printf("%d.%d ", ccc, dcc);
-            }else if ((neg == 1) & (dcc < 10)) {
-                printf("-%d.0%d ", ccc, dcc);
-            } else if ((neg == 1) & (dcc > 10)){
-                printf("-%d.%d ", ccc, dcc);
-            }
-        }
-        
+        printf("%.3lf ", array[i]);
     }
     printf("\n");
     return 0;
