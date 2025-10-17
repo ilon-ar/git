4803024b6f8798d118c0ea6f17f749729919de5f
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-17 16:05:50 +0700

    ex5 try1

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    float A[10][3];
+    float B[3][10];
+    float pr[10][10];
+    int i;
+    int j;
+
+    for (i = 0; i < 10; ++i) {
+        for (j = 0; j < 3; ++j) {
+            scanf("%f", &A[i][j]);
+            printf("%d %d\n", i, j);
+        }
+    }
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 10; ++j) {
+            scanf("%f", &B[i][j]);
+            printf("%d %d\n", i, j);
+        }
+    }
+    for (int p = 0; p < 10; p++) {
+        for (int r = 0; r < 10; r++) {
+            for (int k = 0; k < 3; k++) {
+                pr[p][r] += A[p][k] * B[k][r];
+            }
+        }
+    }
+    for (int p = 0; p < 10; p++) {
+        for (int r = 0; r < 10; r++) {
+            printf("%.3f", pr[p][r]);
+        }
+    }
+    
+    printf("\n");
     return 0;
+
 }
