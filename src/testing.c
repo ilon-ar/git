963c2b337366f274fed7df16b81227e5e2b465f8
КраснобородкинАртемИП-511 <kas.nvk@gmail.com>
2025-10-22 23:43:05 +0700

    ex3 try1

--- src/testing.c
+++ src/testing.c
@@ -3,28 +3,22 @@
 #include "../include/array_function.h"
 
 int main(int argc, char** argv) {
-    int size1 = atoi(argv[1]);
-    int size2 = atoi(argv[2]);
-    int array1[size1];
-    int array2[size2];
-
-    for (int i = 0; i < size1; ++i) {
-        scanf("%d", &array1[i]);
-    }
-   
-    for (int i = 0; i < size2; ++i) {
-        scanf("%d", &array2[i]);
-    }
-    
-    int arrayk[size1 + size2];
-    
-    
-    join_and_sort_int_arrays(array1, size1, array2, size2, arrayk);
+    int size = atoi(argv[1]);
+    int array[size];
 
 
-    for (int i = 0; i < size1 + size2; ++i) {
-        printf("%d ", arrayk[i]);
+    for (int i = 0; i < size; ++i) {
+        scanf("%d", &array[i]);
     }
-    printf("\n");
+    
+    int min;
+    int max;
+    int *mi = &min;
+    int *ma = &max;
+
+    get_min_and_max_from_int_array(array, size, mi, ma);    
+
+
+    printf("%d %d\n", min, max);
     return 0;
 }
